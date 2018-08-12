be44708046b5e1cf5fd89108da2bb598865ea61f
FruitWave <fritzcholar@Wendys-iMac.local>
2018-08-11 21:24:12 -0700

    GamePanel Work
    added a few variables and methods

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,8 +1,38 @@
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
-public class GamePanel {
+import javax.imageio.ImageIO;
+import javax.swing.Timer;
+
+public class GamePanel implements ActionListener {
+	Timer gamespeed;
+	Font font1;
+
+	public GamePanel() {
+		gamespeed = new Timer(1000 / 60, this);
+		font1 = new Font("Arial", 0, 24);
+
+		/*
+		 * try { morrowright =
+		 * ImageIO.read(this.getClass().getResourceAsStream("MorrowRight.jpg")); } catch
+		 * (IOException e) { e.printStackTrace(); }
+		 */
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+	}
+
+	public void startGame() {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 
 	}
 
