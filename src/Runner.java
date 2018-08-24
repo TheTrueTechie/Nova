557e7998c9402149c01d1ea398bff8f7b5791e50
Rohan890 <league@WTS7.attlocal.net>
2018-08-23 18:53:53 -0700

    fixed master bug 001

--- src/Runner.java
+++ src/Runner.java
@@ -7,18 +7,16 @@ public class Runner {
 	static final int width = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
 	static final int height = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
 
-	String gameVersion = ".01 Alpha";
+	String gameVersion = ".02 Alpha";
 
 	GamePanel gamePanel;
 	JFrame mainFrame;
-	JPanel mainPanel;
 
 	public Runner() {
 		gamePanel = new GamePanel();
 
 		mainFrame = new JFrame();
-		mainPanel = new JPanel();
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -27,7 +25,7 @@ public class Runner {
 	}
 
 	void run() {
-		mainFrame.add(mainPanel);
+		mainFrame.add(gamePanel);
 		mainFrame.setTitle("Nova " + gameVersion);
 		mainFrame.setSize(width, height);
 		mainFrame.setVisible(true);
